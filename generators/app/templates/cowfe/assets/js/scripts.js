1bdf7f2158813de2ac3b6f99ac2d54f47d847540
dekisha <dejan@coworks.be>
2017-10-10 15:01:11 +0200

    adding/correcting new stuff.

--- generators/app/templates/cowfe/assets/js/scripts.js
+++ generators/app/templates/cowfe/assets/js/scripts.js
@@ -5,7 +5,7 @@
     attach: function (context, settings) {
 
       $('body', context).once('sampleDrupal8').each(function() {
-        alert("It 's working remove sample from file in: assets/js/scripts.js");
+        alert("If it's working please remove sample from file in: assets/js/scripts.js");
       });
     }
   };
